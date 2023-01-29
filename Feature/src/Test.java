12e10b8ec6229e6c74fba8f891200b8e10ea5bdd
ganesh <ganeshlawange@gmail.com>
2023-01-29 18:20:31 +0530

    Update Test.java
    modify test

--- Feature/src/Test.java
+++ Feature/src/Test.java
@@ -4,5 +4,13 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("this was eclipse file from application...");
+		
+		Student student = new Student();
+		student.setId(101);
+		student.setName("ganesh");
+		student.setAddress("pune");
+		
+		System.out.println(student.getId()+" "+student.getName()+" "+student.getAddress());
+		
 	}
 }
